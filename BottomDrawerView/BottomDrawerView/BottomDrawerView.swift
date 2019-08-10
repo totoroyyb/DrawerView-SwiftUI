92c87d45b70dcc091044b5b075096aec4d9eccdd
TotoroQ <totoroq@outlook.com>
2019-08-10 22:45:59 +0800

    Implement darken background

--- BottomDrawerView/BottomDrawerView/BottomDrawerView.swift
+++ BottomDrawerView/BottomDrawerView/BottomDrawerView.swift
@@ -12,21 +12,33 @@ struct BottomDrawerView: View {
     @Binding var isShow: Bool
     
     var body: some View {
-        VStack {
-            Text("This is drawer")
+        ZStack {
+            if isShow {
+                Rectangle()
+                    .foregroundColor(.black).opacity(0.5)
+                    .animation(.default)
+                    .onTapGesture {
+                        self.isShow.toggle()
+                    }
+            }
+
+            VStack {
+                Text("This is drawer")
+            }
+            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height / 2)
+            .background(Color.blue)
+            .cornerRadius(20)
+            .offset(y: isShow ? UIScreen.main.bounds.height * 0.25 : UIScreen.main.bounds.height * 0.75)
+            .animation(.interpolatingSpring(mass: 0.5, stiffness: 45, damping: 45, initialVelocity: 15))
         }
-        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height / 2)
-        .background(Color.blue)
-        .cornerRadius(20)
-        .offset(y: isShow ? UIScreen.main.bounds.height * 0.25 : UIScreen.main.bounds.height * 0.75)
-        .animation(.interpolatingSpring(mass: 1, stiffness: 50, damping: 50, initialVelocity: 10))
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
 #if DEBUG
 struct BottomDrawerView_Previews: PreviewProvider {
     static var previews: some View {
-        BottomDrawerView(isShow: .constant(true))
+        BottomDrawerView(isShow: .constant(false))
     }
 }
 #endif
